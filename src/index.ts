cef2f730b44d2fd6b2a76e22de8704d09ceeec9b
nanoix9 <nanoix9@gmail.com>
2019-02-20 22:13:33 +0800

    change port

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express from 'express';
+import express from "express";
 
-const port = 8080; // default port to listen
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 // define a route handler for the default home page
 app.get("/hello", (req, res) => {
-    res.send("Hello world!");
+  res.send("Hello world!");
 });
 
 // start the Express server
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
 });
