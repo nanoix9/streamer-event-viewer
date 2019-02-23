22b22a7218885690a2db4963aa9c7143d3cc9fcb
nanoix9 <nanoix9@gmail.com>
2019-02-24 00:08:36 +0800

    implement subscription

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Debug from "debug";
 import express, { Request } from "express";
 import path from "path";
-import request from "request";
+import rp from "request-promise";
 import uuid from "uuid";
 
 const debug = Debug("streamer-event-viewer");
@@ -20,7 +20,7 @@ function _show_req(req: Request) {
     req.protocol,
     req.headers.host,
     req.originalUrl,
-    req.query
+    JSON.stringify(req.query)
   );
 }
 
@@ -31,18 +31,12 @@ app.get("/hello", (req, res) => {
 app.get("/", function(req, res) {
   _show_req(req);
 
-  let loggedIn = false;
-  if (req.query.access_token) {
-    loggedIn = true;
-  }
-
   res.render("pages/index", {
     clientId: CLIENT_ID,
     loginRedirectUrl: `http://localhost:${PORT}`,
     responseType: "token",
     scope: "user:edit",
-    state: uuid.v4(),
-    loggedIn
+    state: uuid.v4()
   });
 });
 
@@ -50,27 +44,77 @@ app.get("/subscribe", function(req, res) {
   _show_req(req);
 
   const uname = req.query.favorteStreamerName;
+  const token = req.query.accessToken;
 
   const opt = {
     url: `https://api.twitch.tv/helix/users?login=${uname}`,
     headers: {
-      "Client-ID": CLIENT_ID
+      "Client-ID": CLIENT_ID,
+      Authorization: `Bearer ${token}`
     }
   };
 
-  function cb(error: any, response: request.Response, body: any) {
-    if (!error && response.statusCode == 200) {
+  debug("send request to", opt);
+  rp(opt)
+    .then(function(body) {
       debug(body);
-      var json = JSON.parse(body);
-      res.render("pages/streamer", {
-        subsribedUsername: json.data[0].login
-      });
-    }
+      const json = JSON.parse(body);
+      const userId = json.data[0].id;
+      return userId;
+    })
+    .then(function(userId) {
+      const opt = {
+        url: "https://api.twitch.tv/helix/webhooks/hub",
+        method: "POST",
+        headers: {
+          "Client-ID": CLIENT_ID,
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        },
+        json: true,
+        body: {
+          "hub.mode": "subscribe",
+          "hub.topic": `https://api.twitch.tv/helix/users/follows?first=1&from_id=${userId}`,
+          "hub.callback": "https://warm-plains-84315.herokuapp.com/onFollow",
+          "hub.lease_seconds": (864000 / 10000) | 0,
+          "hub.secret": "eF2pK3"
+        }
+      };
+      debug("subscribe", opt);
+      return rp(opt);
+    })
+    .then(function() {
+      res.redirect(`/streamer?username=${uname}`);
+    })
+    .catch(function(err) {
+      console.error("failed to subscribe for user", uname, ":", err.message);
+      res.redirect("/error");
+    });
+});
+
+app.get("/onFollow", function(req, res) {
+  _show_req(req);
+  const mode = req.query["hub.mode"];
+  if (mode == "subscribe") {
+    res.send(req.query["hub.challenge"]);
+  } else {
+    res.send("");
+  }
+});
+
+app.get("/streamer", function(req, res) {
+  if (!req.query.username) {
+    res.redirect(`/error?message=empty username`);
+    return;
   }
 
-  debug(opt);
+  res.render("pages/streamer", {
+    subsribedUsername: req.query.username
+  });
+});
 
-  request(opt, cb);
+app.get("/error", function(req, res) {
+  res.render("pages/404", { message: req.query.message });
 });
 
 // start the Express server
